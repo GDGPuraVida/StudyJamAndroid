7319f20fb4243ae89363307cfb472ff1f250e662
Eduardo Oviedo Blanco <eduardo.oviedo@gmail.com>
2016-04-15 23:18:22 -0600

    First class practice, first project and second class practice

---
+++ HolaMundo/app/src/main/java/com/patito/eduardo/holamundo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.patito.eduardo.holamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
